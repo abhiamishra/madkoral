3c852eeba2510745eb6f4ceed023b00402fcd77c
Dongji Yang <dongji.yang@outlook.com>
2022-04-26 19:12:18 -0500

    init

---
+++ sql_scripts/create_tables.sql
@@ -0,0 +1,39 @@
+-- create database themepark;
+
+use themepark;
+
+-- create table Department(
+-- 	DeptID int,
+--     DeptType varchar(300),
+--     Supervisor varchar(300),
+--     primary key(DeptID)
+-- );
+
+-- create table Rides(
+-- 	deptID int,
+--     showID int,
+--     attractionName varchar(300),
+--     skill_lvl int,
+--     primary key(deptID)
+-- );
+
+-- create table Security(
+-- 	deptID int,
+--     physicalIndex int,
+--     primary key(deptID)
+-- );
+
+-- create table Leisure(
+-- 	deptID int,
+--     hosp_index int,
+--     primary key(deptID)
+-- );
+
+-- create table Administration(
+-- 	deptID int,
+--     WPM int,
+--     primary key(deptID)
+-- );
+
+
+
