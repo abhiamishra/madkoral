941f577736ca8e1f8c3b5a424208e92a125a421f
Dongji Yang <dongji.yang@outlook.com>
2022-04-26 21:36:51 -0500

    python script to load db

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # madkoral
 CS 4347 Repository for the Mad Koral theme park
+
+python_scripts:
+    pip install mysql-connector-python
---
+++ python_scripts/static.py
@@ -0,0 +1,47 @@
+passes = ['bronze', 'silver', 'gold', 'platinum']
+
+rideTypes = ['merry-go-round', 'rollercoaster', 'funhouse', 'ferris-wheel', 'pendulum ride']
+
+verbs = [
+    'destroyer',
+    'hunter',
+    'dreamer',
+    'concern',
+    'emerge',
+    'isolate',
+    'wrap',
+    'import',
+    'stem',
+    'focus'
+]
+
+nouns = [
+    'psychology',
+    'poetry',
+    'health',
+    'transportation',
+    'region',
+    'instruction',
+    'magazine',
+    'device',
+    'republic',
+    'analysis'
+]
+
+tableNames = [
+    'administration',
+    'attraction',
+    'child',
+    'department',
+    'destination',
+    'employee',
+    'guest',
+    'leisure',
+    'rides',
+    'rides_dept',
+    'security',
+    'shows',
+    'visits',
+    'watches',
+    'works_in'
+]
